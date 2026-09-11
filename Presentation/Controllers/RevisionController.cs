2535efe0ee8023e4f02255f28ea8e3b089c68950
ABDUL-AZEEZ OLANREWAJU OKUNLOLA <froshkideaglet@gmail.com>
2026-09-11 15:47:52 +0100

    Make Revision routing explicit and add antiforgery token; ensure form posts to Revision/Revise

--- Presentation/Controllers/RevisionController.cs
+++ Presentation/Controllers/RevisionController.cs
@@ -4,22 +4,23 @@ using System.Threading.Tasks;
 
 namespace Presentation.Controllers
 {
+    [Route("Revision")]
     public class RevisionController : Controller
     {
         private readonly IPythonApiClient _api;
         public RevisionController(IPythonApiClient api) => _api = api;
 
-        [HttpGet]
+        [HttpGet("")]
         public IActionResult Index()
         {
             return View();
         }
 
-        [HttpPost]
+        [HttpPost("Revise")]
         public async Task<IActionResult> Revise(string inputText)
         {
             var resp = await _api.ReviseAsync(new ReviseRequest { Text = inputText });
-            ViewBag.Result = resp?.RevisedText ?? "No response from Python API.";
+            ViewBag.Result = resp?.RevisedText ?? "No response from backend.";
             ViewBag.Input = inputText;
             return View("Index");
         }
